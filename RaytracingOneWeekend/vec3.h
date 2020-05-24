ccbad18b2f57ec8640a7d9c8579c4deb0f1b4744
Belpaire <belpairebram@hotmail.com>
2020-05-24 22:07:19 +0200

    State before new main

--- RaytracingOneWeekend/vec3.h
+++ RaytracingOneWeekend/vec3.h
@@ -12,26 +12,62 @@ public:
 		arr[2] = z;
 	}
 
-	constexpr inline double x() const { return arr[0]; }
-	constexpr inline double y() const { return arr[1]; }
-	constexpr inline double z() const  { return arr[2]; }
-	constexpr inline double r()const  { return arr[0]; }
-	constexpr inline double g()const { return arr[1]; }
-	constexpr inline double b()const { return arr[2]; }
+	constexpr  double x() const { return arr[0]; }
+	constexpr  double y() const { return arr[1]; }
+	constexpr  double z() const  { return arr[2]; }
+	constexpr  double r()const  { return arr[0]; }
+	constexpr  double g()const { return arr[1]; }
+	constexpr  double b()const { return arr[2]; }
 
-	constexpr inline vec3& operator+()  { return *this; }
+	constexpr  vec3& operator+()  { return *this; }
 
 	inline vec3 operator-() const { return vec3(-arr[0],-arr[1],-arr[2]); }
-	constexpr inline double operator[](int i) const { return arr[i]; }
-	constexpr inline double& operator[](int i) { return arr[i]; }
+	constexpr  double operator[](int i) const { return arr[i]; }
+	constexpr  double& operator[](int i) { return arr[i]; }
 
 
-	inline vec3& operator+=(const vec3 &v2);
-	inline vec3& operator-=(const vec3& v2);
-	inline vec3& operator*=(const vec3& v2);
-	inline vec3& operator/=(const vec3& v2);
-	inline vec3& operator*=(const double t);
-	inline vec3& operator/=(const double t);
+	inline vec3& operator+=(const vec3& v2) {
+		arr[0] += v2[0];
+		arr[1] += v2[1];
+		arr[2] += v2[2];
+		return *this;
+	}
+	inline vec3& operator-=(const vec3& v2) {
+		arr[0] -= v2[0];
+		arr[1] -= v2[1];
+		arr[2] -= v2[2];
+		return *this;
+	
+	}
+	inline vec3& operator*=(const vec3& v2) {
+		arr[0] *= v2[0];
+		arr[1] *= v2[1];
+		arr[2] *= v2[2];
+		return *this;
+	
+	}
+	inline vec3& operator/=(const vec3& v2) {
+		arr[0] /= v2[0];
+		arr[1] /= v2[1];
+		arr[2] /= v2[2];
+		return *this;
+
+	}
+	inline vec3& operator*=(const double t){
+		arr[0] *= t;
+		arr[1] *= t;
+		arr[2] *= t;
+		return *this;
+
+	}
+	inline vec3& operator/=(const double t) {
+		arr[0] /= t;
+		arr[1] /= t;
+		arr[2] /= t;
+		return *this;
+		
+
+	}
 
 	inline double length() const {
 		return sqrt(arr[0]* arr[0] + arr[1]* arr[1] + arr[2]* arr[2]);
@@ -40,7 +76,6 @@ public:
 	inline double squaredLength() const {
 		return arr[0] * arr[0] + arr[1] * arr[1] + arr[2] * arr[2];
 	}
-	inline void make_unit_vector();
 };
 
 
@@ -73,3 +108,20 @@ inline vec3 operator*(const vec3& v, double t) {
 inline vec3 operator/(vec3 v, double t) {
 	return (1 / t) * v;
 }
+
+inline double dot(const vec3& u, const vec3& v) {
+	return u.arr[0] * v.arr[0]
+		+ u.arr[1] * v.arr[1]
+		+ u.arr[2] * v.arr[2];
+}
+
+inline vec3 cross(const vec3& u, const vec3& v) {
+	return vec3(u.arr[1] * v.arr[2] - u.arr[2] * v.arr[1],
+		u.arr[2] * v.arr[0] - u.arr[0] * v.arr[2],
+		u.arr[0] * v.arr[1] - u.arr[1] * v.arr[0]);
+}
+
+inline vec3 unit_vector(vec3 v) {
+	return v / v.length();
+}
+
